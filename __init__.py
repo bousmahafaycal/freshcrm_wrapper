6f8dca19beffc6d2f13f35f95f78d661ef21d482
Faycal Bousmaha <faycal.bousmaha.fac@gmail.com>
2021-01-28 13:10:09 +0100

    Fix init

--- __init__.py
+++ __init__.py
@@ -1,3 +1,1 @@
-__author__ = 'Bhagirath Goud <bhagirath@freshdesk.com>'
-__license__ = 'MIT'
-__version__ = '1.0.0'
+
---
+++ freshsales/__init__.py
@@ -0,0 +1,3 @@
+__author__ = 'Bhagirath Goud <bhagirath@freshdesk.com>'
+__license__ = 'MIT'
+__version__ = '1.0.0'
