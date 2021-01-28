54c8152e7315c6a7aea9e5fbed1fca83181be3f7
Faycal Bousmaha <faycal.bousmaha.fac@gmail.com>
2021-01-28 13:16:52 +0100

    Fix init;

--- freshsales/__init__.py
+++ freshsales/__init__.py
@@ -2,4 +2,4 @@ __author__ = 'Bhagirath Goud <bhagirath@freshdesk.com>'
 __license__ = 'MIT'
 __version__ = '1.0.0'
 
-from freshsales_analytics import FreshsalesAnalytics
+from .freshsales_analytics import FreshsalesAnalytics
