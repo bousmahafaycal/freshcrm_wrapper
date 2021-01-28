bd3129bccf8675fbc36a71292e92a1d10e0841ea
Faycal Bousmaha <faycal.bousmaha.fac@gmail.com>
2021-01-28 12:47:14 +0100

    Freshsales is now a package on github

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
-from .freshsales_analytics import FreshsalesAnalytics
-
 __author__ = 'Bhagirath Goud <bhagirath@freshdesk.com>'
 __license__ = 'MIT'
 __version__ = '1.0.0'
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup (
+    name='Freshsales',
+    url='https://github.com/jladan/package_demo',
+    author='Bhagirath Goud',
+    author_email='bhagirath@freshdesk.com',
+    # Needed to actually package something
+    packages=['measure'],
+    # Needed for dependencies
+    install_requires=['numpy'],
+    # *strongly* suggested for sharing
+    version='1.0.0',
+    # The license can be anything you like
+    license='MIT',
+    description='Freshsales python package',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
